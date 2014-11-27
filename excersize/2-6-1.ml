8859af521e27e8c1ab3d63da01e2cd77e06aa61a
tyage <namatyage@gmail.com>
2014-11-27 15:33:07 +0900

    added comment

--- excersize/2-6-1.ml
+++ excersize/2-6-1.ml
@@ -1,3 +1,9 @@
+(*
+US ドル (実数) を受け取って円 (整数) に換算する関数 (ただし 1 円以下四捨五入)．
+(入力は小数点以下 2 桁で終わるときに働けばよい．)
+レートは 1$ = 111.12 円とする．
+*)
+
 let dollar_to_yen(dollar : float) : int =
   let yen = dollar *. 112.12 in
   let carry = if ((yen -. floor(yen)) > 0.5) then 1 else 0 in
--- excersize/2-6-2.ml
+++ excersize/2-6-2.ml
@@ -1,3 +1,8 @@
+(*
+円 (整数) を受け取って，US ドル (セント以下を小数にした実数) に換算する関数 (ただし 1 セント以下四捨五入)．
+レートは 1$ = 111.12 円とする．
+*)
+
 let yen_to_dollar(yen : int) : float =
   let dollar = float_of_int(yen) /. 112.12 *. 100.0 in
   let carry = if ((dollar -. floor(dollar)) > 0.5) then 1 else 0 in
