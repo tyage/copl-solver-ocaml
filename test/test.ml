0e40ecbba4d8658c573c41c75bc9544aaed5a0df
tyage <namatyage@gmail.com>
2014-12-04 16:22:37 +0900

    四捨五入のテスト

--- test/test.ml
+++ test/test.ml
@@ -2,10 +2,10 @@ open OUnit;;
 
 let ex_2_6_1 =
   let test1 () = assert_equal (Ex_2_6_1.dollar_to_yen 100.0) 11212 in
-  let test2 () = assert_equal "x" "x" in
+  let test2 () = assert_equal (Ex_2_6_1.dollar_to_yen 49.0) 5494  in
   "ex2_6_1">:::
-  ["test1">:: test1;
-  "test2">:: test2]
+  ["四捨">:: test1;
+  "五入">:: test2]
 ;;
 
 let _ =
