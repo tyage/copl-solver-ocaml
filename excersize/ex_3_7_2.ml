b2cec8c434d8563b51460f5e0ce7344fe8a88dd3
tyage <namatyage@gmail.com>
2014-12-05 15:09:40 +0900

    3.7.2の引数の取り方を変更

--- excersize/ex_3_7_2.ml
+++ excersize/ex_3_7_2.ml
@@ -1,7 +1,7 @@
-let rec pow(x, n) : float =
+let rec pow x n =
   if n == 0 then
     1.0
   else if n mod 2 == 0 then
-    pow(x *. x, n / 2)
+    (pow (x *. x) (n / 2))
   else
-    pow(x, n - 1) *. x;;
+    (pow x (n - 1)) *. x;;
