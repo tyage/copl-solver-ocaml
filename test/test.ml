ecdfcdd1793cd025d58dbd7aaf64b3ed6a907c97
tyage <namatyage@gmail.com>
2014-12-05 15:07:20 +0900

    test for 3.7.1

--- test/test.ml
+++ test/test.ml
@@ -3,7 +3,7 @@ open OUnit;;
 let ex_2_6_1 =
   let test1 () = assert_equal (Ex_2_6_1.dollar_to_yen 100.0) 11212 in
   let test2 () = assert_equal (Ex_2_6_1.dollar_to_yen 49.0) 5494 in
-  "ex2_6_1">:::
+  "ex_2_6_1">:::
   ["四捨">:: test1;
   "五入">:: test2]
 ;;
@@ -11,7 +11,7 @@ let ex_2_6_1 =
 let ex_2_6_2 =
   let test1 () = assert_equal (Ex_2_6_2.yen_to_dollar 10000) 89.19 in
   let test2 () = assert_equal (Ex_2_6_2.yen_to_dollar 10002) 89.21 in
-  "ex2_6_1">:::
+  "ex_2_6_2">:::
   ["四捨">:: test1;
   "五入">:: test2]
 ;;
@@ -19,7 +19,7 @@ let ex_2_6_2 =
 let ex_2_6_3 =
   let test1 () = assert_equal (Ex_2_6_3.say_yen 100.0) "100. dollars are 11212 yen." in
   let test2 () = assert_equal (Ex_2_6_3.say_yen 49.0) "49. dollars are 5494 yen." in
-  "ex2_6_1">:::
+  "ex_2_6_3">:::
   ["四捨">:: test1;
   "五入">:: test2]
 ;;
@@ -27,9 +27,15 @@ let ex_2_6_3 =
 let ex_2_6_4 =
   let cap () = assert_equal (Ex_2_6_4.capitalize 'h') 'H' in
   let not_cap () = assert_equal (Ex_2_6_4.capitalize '1') '1' in
-  "ex2_6_1">:::
+  "ex_2_6_4">:::
   ["capitalize">:: cap;
   "not capitalize">:: not_cap]
+;;
+
+let ex_3_7_1 =
+  let pow () = assert_equal (Ex_3_7_1.pow 4.0 2) 16.0 in
+  "ex_3_7_1">:::
+  ["pow">:: pow]
 ;;
 
 let ex_3_11 =
@@ -37,7 +43,7 @@ let ex_3_11 =
   let comb () = assert_equal (Ex_3_11.comb 5 2) 10 in
   let fib () = assert_equal (Ex_3_11.fib_iter 10) 55 in
   let max_ascii () = assert_equal (Ex_3_11.max_ascii "jvDzZe") 'z' in
-  "ex2_6_1">:::
+  "ex_3_11">:::
   ["gcd">:: gcd;
   "comb">:: comb;
   "fib">:: fib;
@@ -49,5 +55,6 @@ run_test_tt_main ex_2_6_1;
 run_test_tt_main ex_2_6_2;
 run_test_tt_main ex_2_6_3;
 run_test_tt_main ex_2_6_4;
+run_test_tt_main ex_3_7_1;
 run_test_tt_main ex_3_11;
 ;;
