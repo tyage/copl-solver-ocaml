4ae122b2928667c728197fa0b5ad186c94e0fc37
tyage <namatyage@gmail.com>
2014-12-05 14:53:04 +0900

    test for 2.6.3

--- test/test.ml
+++ test/test.ml
@@ -2,7 +2,7 @@ open OUnit;;
 
 let ex_2_6_1 =
   let test1 () = assert_equal (Ex_2_6_1.dollar_to_yen 100.0) 11212 in
-  let test2 () = assert_equal (Ex_2_6_1.dollar_to_yen 49.0) 5494  in
+  let test2 () = assert_equal (Ex_2_6_1.dollar_to_yen 49.0) 5494 in
   "ex2_6_1">:::
   ["四捨">:: test1;
   "五入">:: test2]
@@ -10,12 +10,22 @@ let ex_2_6_1 =
 
 let ex_2_6_2 =
   let test1 () = assert_equal (Ex_2_6_2.yen_to_dollar 10000) 89.19 in
-  let test2 () = assert_equal (Ex_2_6_2.yen_to_dollar 10002) 89.21  in
+  let test2 () = assert_equal (Ex_2_6_2.yen_to_dollar 10002) 89.21 in
+  "ex2_6_1">:::
+  ["四捨">:: test1;
+  "五入">:: test2]
+;;
+
+let ex_2_6_3 =
+  let test1 () = assert_equal (Ex_2_6_3.say_yen 100.0) "100. dollars are 11212 yen." in
+  let test2 () = assert_equal (Ex_2_6_3.say_yen 49.0) "49. dollars are 5494 yen." in
   "ex2_6_1">:::
   ["四捨">:: test1;
   "五入">:: test2]
 ;;
 
 let _ =
-run_test_tt_main ex_2_6_1
+run_test_tt_main ex_2_6_1;
+run_test_tt_main ex_2_6_2;
+run_test_tt_main ex_2_6_3;
 ;;
