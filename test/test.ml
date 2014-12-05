63ee273d91a6efa5fa08d635e1dcd892733479f3
tyage <namatyage@gmail.com>
2014-12-05 15:29:58 +0900

    use OUnit2

--- test/test.ml
+++ test/test.ml
@@ -1,54 +1,54 @@
-open OUnit;;
+open OUnit2;;
 
 let ex_2_6_1 =
-  let test1 () = assert_equal (Ex_2_6_1.dollar_to_yen 100.0) 11212 in
-  let test2 () = assert_equal (Ex_2_6_1.dollar_to_yen 49.0) 5494 in
+  let test1 _ = assert_equal (Ex_2_6_1.dollar_to_yen 100.0) 11212 in
+  let test2 _ = assert_equal (Ex_2_6_1.dollar_to_yen 49.0) 5494 in
   "ex_2_6_1">:::
   ["四捨">:: test1;
   "五入">:: test2]
 ;;
 
 let ex_2_6_2 =
-  let test1 () = assert_equal (Ex_2_6_2.yen_to_dollar 10000) 89.19 in
-  let test2 () = assert_equal (Ex_2_6_2.yen_to_dollar 10002) 89.21 in
+  let test1 _ = assert_equal (Ex_2_6_2.yen_to_dollar 10000) 89.19 in
+  let test2 _ = assert_equal (Ex_2_6_2.yen_to_dollar 10002) 89.21 in
   "ex_2_6_2">:::
   ["四捨">:: test1;
   "五入">:: test2]
 ;;
 
 let ex_2_6_3 =
-  let test1 () = assert_equal (Ex_2_6_3.say_yen 100.0) "100. dollars are 11212 yen." in
-  let test2 () = assert_equal (Ex_2_6_3.say_yen 49.0) "49. dollars are 5494 yen." in
+  let test1 _ = assert_equal (Ex_2_6_3.say_yen 100.0) "100. dollars are 11212 yen." in
+  let test2 _ = assert_equal (Ex_2_6_3.say_yen 49.0) "49. dollars are 5494 yen." in
   "ex_2_6_3">:::
   ["四捨">:: test1;
   "五入">:: test2]
 ;;
 
 let ex_2_6_4 =
-  let cap () = assert_equal (Ex_2_6_4.capitalize 'h') 'H' in
-  let not_cap () = assert_equal (Ex_2_6_4.capitalize '1') '1' in
+  let cap _ = assert_equal (Ex_2_6_4.capitalize 'h') 'H' in
+  let not_cap _ = assert_equal (Ex_2_6_4.capitalize '1') '1' in
   "ex_2_6_4">:::
   ["capitalize">:: cap;
   "not capitalize">:: not_cap]
 ;;
 
 let ex_3_7_1 =
-  let pow () = assert_equal (Ex_3_7_1.pow 4.0 2) 16.0 in
+  let pow _ = assert_equal (Ex_3_7_1.pow 4.0 2) 16.0 in
   "ex_3_7_1">:::
   ["pow">:: pow]
 ;;
 
 let ex_3_7_2 =
-  let pow () = assert_equal (Ex_3_7_2.pow 4.0 2) 16.0 in
+  let pow _ = assert_equal (Ex_3_7_2.pow 4.0 2) 16.0 in
   "ex_3_7_2">:::
   ["pow">:: pow]
 ;;
 
 let ex_3_11 =
-  let gcd () = assert_equal (Ex_3_11.gcd 35 14) 7 in
-  let comb () = assert_equal (Ex_3_11.comb 5 2) 10 in
-  let fib () = assert_equal (Ex_3_11.fib_iter 10) 55 in
-  let max_ascii () = assert_equal (Ex_3_11.max_ascii "jvDzZe") 'z' in
+  let gcd _ = assert_equal (Ex_3_11.gcd 35 14) 7 in
+  let comb _ = assert_equal (Ex_3_11.comb 5 2) 10 in
+  let fib _ = assert_equal (Ex_3_11.fib_iter 10) 55 in
+  let max_ascii _ = assert_equal (Ex_3_11.max_ascii "jvDzZe") 'z' in
   "ex_3_11">:::
   ["gcd">:: gcd;
   "comb">:: comb;
@@ -58,20 +58,20 @@ let ex_3_11 =
 
 let ex_4_1 =
   let pi = 4.0 *. atan 1.0 in
-  let calc_pi () = assert_equal (int_of_float ((Ex_4_1.integral sin 0.0 pi) +. 0.1)) 2 in
+  let calc_pi _ = assert_equal (int_of_float ((Ex_4_1.integral sin 0.0 pi) +. 0.1)) 2 in
   "ex_3_11">:::
   ["calc_pi">:: calc_pi]
 ;;
 
 let ex_4_4 =
   let avg = Ex_4_4.uncurry Ex_4_4.curried_avg in
-  let calc_avg () = assert_equal (avg (4.0, 5.3)) 4.65 in
+  let calc_avg _ = assert_equal (avg (4.0, 5.3)) 4.65 in
   "ex_3_11">:::
   ["calc_avg">:: calc_avg]
 ;;
 
 let ex_4_5 =
-  let fib () = assert_equal (Ex_4_5.fib 10) 55 in
+  let fib _ = assert_equal (Ex_4_5.fib 10) 55 in
   "ex_3_11">:::
   ["fib">:: fib]
 ;;
