35d0767ac90678e35f1cda328b3fd3f1d0725ea9
tyage <namatyage@gmail.com>
2015-01-09 09:41:11 +0900

    fixed a indent caused bug :(

--- interpreter/eval.ml
+++ interpreter/eval.ml
@@ -14,6 +14,7 @@ let err s = raise (Error s)
 let rec string_of_exval = function
     IntV i -> string_of_int i
   | BoolV b -> string_of_bool b
+  | ProcV (_, _, _) -> "<fun>"
 
 let pp_val v = print_string (string_of_exval v)
 
@@ -50,13 +51,13 @@ let rec eval_exp env = function
           eval_exp (Environment.extend id value env) exp2
   | FunExp (id, exp) -> ProcV (id, exp, env)
   | AppExp (exp1, exp2) ->
-    let funval = eval_exp env exp2 in
+    let funval = eval_exp env exp1 in
     let arg = eval_exp env exp2 in
       (match funval with
           ProcV (id, body, env') ->
             let newenv = Environment.extend id arg env' in
               eval_exp newenv body
-          | _ -> err ("Non-function value is applied"))
+        | _ -> err ("Non-function value is applied"))
 
 let eval_decl env = function
     Exp e -> let v = eval_exp env e in ("-", env, v)
